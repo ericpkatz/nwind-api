0d9fa2bee860df1073301a89890532c9022d20f2
Eric P. Katz <ericpkatz@gmail.com>
2016-05-29 18:27:35 -0400

    move logic to SessionFactory

--- browser/javascript/login.ctrl.js
+++ browser/javascript/login.ctrl.js
@@ -1,39 +1,14 @@
 angular.module('app')
-  .controller('LoginCtrl', function($scope, SessionFactory, $http, $window){
-          $scope.login = function(form){
-            $scope.$broadcast('schemaFormValidate'); 
-            if(form.$valid)
-              SessionFactory.create($scope.model)
-                .then(function(session){
-                  $window.sessionStorage.setItem('token', session.id); 
-                  return $http.get('/api/sessions/' + $window.sessionStorage.getItem('token')); 
-                })
-                .then(function(response){
-                  angular.copy(response.data, SessionFactory.auth);
-                });
-          };
-          $scope.schema = {
-            type: 'object',
-            properties: {
-              email: {
-                type: 'string'
-              },
-              password: {
-                type: 'string'
-              } 
-            },
-            required: ['email', 'password']
-          
-          };
-          $scope.form = [
-            '*',
-            {
-              type: 'submit',
-              title: 'Login'
-            
-            }
-          ];
-          $scope.model = {
-          
-          };
+  .controller('LoginCtrl', function($scope, SessionFactory, $state){
+    $scope.login = function(form){
+      $scope.$broadcast('schemaFormValidate'); 
+      if(form.$valid)
+        SessionFactory.login($scope.model)
+          .then(function(){
+            $state.go('departments');
+          });
+    };
+    $scope.schema = SessionFactory.schema;
+    $scope.form = SessionFactory.form;
+    $scope.model = { };
   });
--- browser/javascript/session.factory.js
+++ browser/javascript/session.factory.js
@@ -1,12 +1,47 @@
 angular.module('app')
-  .factory('SessionFactory', function(DS){
+  .factory('SessionFactory', function(DS, $window, $http){
     var factory = DS.defineResource({
       name: 'session',
       endpoint: 'sessions'
     });
 
+    factory.login = function(credentials){
+
+      return this.create(credentials)
+        .then(function(session){
+          $window.sessionStorage.setItem('token', session.id); 
+          return $http.get('/api/sessions/' + $window.sessionStorage.getItem('token')); 
+        })
+        .then(function(response){
+          angular.copy(response.data, factory.auth);
+        });
+    };
+
     factory.auth = {
     };
+
+    factory.schema = {
+      type: 'object',
+      properties: {
+        email: {
+          type: 'string'
+        },
+        password: {
+          type: 'string'
+        } 
+      },
+      required: ['email', 'password']
+    
+    };
+
+    factory.form = [
+      '*',
+      {
+        type: 'submit',
+        title: 'Login'
+      
+      }
+    ];
 
     return factory;
 
