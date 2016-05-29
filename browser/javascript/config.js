d4a485109e70a43eb0c6f530be7c7c6b019c5fc7
Eric P. Katz <ericpkatz@gmail.com>
2016-05-29 08:05:58 -0400

    some refactor

--- browser/javascript/config.js
+++ browser/javascript/config.js
@@ -75,45 +75,6 @@ angular.module('app')
       .state('login', {
         url: '/login',
         templateUrl: '/browser/templates/login.html',
-        controller: function($scope, SessionFactory, $http, $window){
-          $scope.login = function(form){
-            $scope.$broadcast('schemaFormValidate'); 
-            if(form.$valid)
-              SessionFactory.create($scope.model)
-                .then(function(session){
-                  $window.sessionStorage.setItem('token', session.id); 
-                  return $http.get('/api/sessions/' + $window.sessionStorage.getItem('token')); 
-                })
-                .then(function(response){
-                  angular.copy(response.data, SessionFactory.auth);
-                });
-          };
-          $scope.schema = {
-            type: 'object',
-            properties: {
-              email: {
-                type: 'string'
-              },
-              password: {
-                type: 'string'
-              } 
-            },
-            required: ['email', 'password']
-          
-          };
-          $scope.form = [
-            '*',
-            {
-              type: 'submit',
-              title: 'Login'
-            
-            }
-          ];
-          $scope.model = {
-          
-          };
-        
-        }
-      
+        controller: 'LoginCtrl',
       });
   });
