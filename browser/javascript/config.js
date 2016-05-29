7fe74c51df13f285db63178551d9ef1c61f5d87f
Eric P. Katz <ericpkatz@gmail.com>
2016-05-29 07:59:22 -0400

    department edit ctrl

--- browser/javascript/config.js
+++ browser/javascript/config.js
@@ -26,55 +26,7 @@ angular.module('app')
       .state('departments.edit', {
         url: '/:id/edit',
         templateUrl: '/browser/templates/edit.department.html',
-        controller: function($state, $scope, department, DepartmentFactory){
-          $scope.form = [
-            "name",
-            "priority",
-            {
-              "type": "submit",
-              "style": "btn-info",
-              "title": "OK"
-            },
-            {
-              "type": "button",
-              "style": "btn",
-              "title": "Cancel",
-              "onClick": "cancel()"
-            }
-          ];
-          $scope.schema = {
-            "type": "object",
-            "title": "Department",
-            "properties": {
-              "name": {
-                "title": "Name",
-                "type": "string"
-              },
-              "priority": {
-                "title": "Priority",
-                "type": "integer",
-                "minimum": 1,
-                "maximum": 15
-              }
-            },
-            "required": [
-              "name"
-            ]
-          }; 
-
-          $scope.model = angular.copy(department);
-
-          $scope.cancel = function(){
-            $scope.model = angular.copy(department);
-          };
-
-          $scope.save = function(){
-            DepartmentFactory.update(department.id, $scope.model )
-              .then(function(){
-                $state.go('departments');
-              });
-          };
-        },
+        controller: 'DepartmentEditCtrl',
         resolve: {
           department: function(DepartmentFactory, $stateParams){
             return DepartmentFactory.find($stateParams.id);
